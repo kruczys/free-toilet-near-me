16bb5c0f2efeb07f36d0ef9729b56be3aa7a1935
Paw565 <paw565pl@gmail.com>
2023-07-23 14:34:32 +0200

    built basic search bar component

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,11 +1,12 @@
-import "./style.css";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
-    <div className="px-5 py-4 shadow-sm bg-slate-50">
+    <div className="flex items-center px-5 py-4 shadow-sm bg-slate-50">
       <h1 className="text-3xl font-bold tracking-wide text-orange-900">
         FreeToilet
       </h1>
+      <SearchBar></SearchBar>
     </div>
   );
 };
---
+++ src/components/Navbar/SearchBar.tsx
@@ -0,0 +1,14 @@
+const SearchBar = () => {
+  return (
+    <form>
+      <input
+        className="rounded-xl border-2"
+        type="text"
+        name="location-search"
+        id="location-search"
+      />
+    </form>
+  );
+};
+
+export default SearchBar;
