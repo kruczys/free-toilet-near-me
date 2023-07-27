0c2f73719f9ed4aadf2dac5084a98828d9af4595
Konrad Kreczko <konradkreczko@gmail.com>
2023-07-27 23:55:09 +0200

    Added typescript support for react leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import NavBar from "./components/Navbar";
+import MainMap from "./components/Map/MainMap.tsx";
 
 const App = () => {
-  return <NavBar></NavBar>;
+  return (
+    <div>
+      <NavBar></NavBar>
+      <MainMap></MainMap>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Map/MainMap.tsx
@@ -0,0 +1,11 @@
+// import { MapContainer } from "https://cdn.esm.sh/react-leaflet/MapContainer";
+// import { TileLayer } from "https://cdn.esm.sh/react-leaflet/TileLayer";
+// import { useMap } from "https://cdn.esm.sh/react-leaflet/hooks";
+
+const MainMap = () => {
+  // const map = 0;
+
+  return <div></div>;
+};
+
+export default MainMap;
