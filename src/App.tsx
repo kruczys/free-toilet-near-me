fea0bcd06bc0deb42588076195845b4ddd2cf7d1
Konrad Kreczko <konradkreczko@gmail.com>
2023-07-28 17:58:24 +0200

    Added basic map, there are some import issues, will fix later :rofl:

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import NavBar from "./components/Navbar";
 import MainMap from "./components/Map/MainMap.tsx";
+import "leaflet/dist/leaflet.css";
 
 const App = () => {
-  return (
-    <div>
-      <NavBar></NavBar>
-      <MainMap></MainMap>
-    </div>
-  );
+  return <MainMap></MainMap>;
 };
 
 export default App;
--- src/components/Map/MainMap.tsx
+++ src/components/Map/MainMap.tsx
@@ -1,11 +1,23 @@
-// import { MapContainer } from "https://cdn.esm.sh/react-leaflet/MapContainer";
-// import { TileLayer } from "https://cdn.esm.sh/react-leaflet/TileLayer";
+import { MapContainer } from "https://cdn.esm.sh/react-leaflet/MapContainer";
+import { TileLayer } from "https://cdn.esm.sh/react-leaflet/TileLayer";
 // import { useMap } from "https://cdn.esm.sh/react-leaflet/hooks";
 
 const MainMap = () => {
-  // const map = 0;
+  const cords = [54.38528573727253, 18.609002982471225];
 
-  return <div></div>;
+  return (
+    <MapContainer
+      center={cords}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="h-80"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
 };
 
 export default MainMap;
