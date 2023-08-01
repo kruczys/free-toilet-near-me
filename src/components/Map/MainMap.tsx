7a20d44918463969467965a1cdc7908cb8195ff1
Konrad Kreczko <konradkreczko@gmail.com>
2023-08-01 15:49:51 +0200

    added function that creates Markers on map

--- src/components/Map/MainMap.tsx
+++ src/components/Map/MainMap.tsx
@@ -1,11 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const MainMap = () => {
-  const cords = [54.38528573727253, 18.609002982471225];
+  const testCords1 = [54.396244853612004, 18.57431564599356];
+  const testCords2 = [54.423398828203716, 18.594126803452735];
+  const centerCords = [54.38528573727253, 18.609002982471225];
+  const cords = [testCords1, testCords2];
 
   return (
     <MapContainer
-      center={{ lat: cords[0], lng: cords[1] }}
+      center={{ lat: centerCords[0], lng: centerCords[1] }}
       zoom={13}
       className="h-screen"
     >
@@ -13,9 +16,13 @@ const MainMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={{ lat: cords[0], lng: cords[1] }}>
-        <Popup>Test Popup</Popup>
-      </Marker>
+      {cords.map((cord) => (
+        <Marker position={{ lat: cord[0], lng: cord[1] }}>
+          <Popup>
+            {cord[0]} {cord[1]}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
