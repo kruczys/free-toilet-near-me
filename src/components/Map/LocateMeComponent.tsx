b54ff7b9ab17a848816628884e5bc2facf5a8dac
Konrad Kreczko <konradkreczko@gmail.com>
2023-08-02 17:56:44 +0200

    added locateme component, needs fixin

--- src/components/Map/LocateMeComponent.tsx
+++ src/components/Map/LocateMeComponent.tsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
-import CustomMarker from "./CustomMarker.tsx";
+import { useMapEvents } from "react-leaflet";
+import { Marker } from "react-leaflet";
 
 const locateMeComponent = () => {
   const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : <Marker position={position}></Marker>;
 };
 
 export default locateMeComponent;
