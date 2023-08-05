159118a55a4120b8c62be11b147d45e485875f2d
Paw565 <paw565pl@gmail.com>
2023-08-05 19:08:19 +0200

    added rating component

--- src/components/Map/ToiletMarker.tsx
+++ src/components/Map/ToiletMarker.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "leaflet";
 import { Marker, Popup } from "react-leaflet";
+import { Rating } from "react-simple-star-rating";
 import Toilet from "../../Entities/Toilet.js";
 import toiletIcon from "../../assets/Icons/toilet.svg";
 
@@ -18,10 +19,18 @@ const CustomMarker = ({
   return (
     <Marker position={{ lat, lng }} icon={markerIcon}>
       <Popup>
-        <h2 className="text-xl">{name}</h2>
+        <h2 className="text-xl mb-1 font-bold">{name}</h2>
         <div>{price === 0 ? "Free" : `${price} PLN`}</div>
-        <div>{rating}</div>
-        <div className="flex justify-around">
+        <div>
+          <Rating
+            size={25}
+            allowFraction={true}
+            SVGstyle={{ display: "inline" }}
+            initialValue={rating !== 0 ? rating : 0}
+            transition={true}
+          />
+        </div>
+        <div className="flex justify-around mt-2">
           <a
             href={`https://www.google.com/maps/dir/?api=1&destination=${lat}%2C${lng}`}
             className="underline"
