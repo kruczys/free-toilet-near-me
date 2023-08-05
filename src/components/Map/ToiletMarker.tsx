f499b69cf73473aa1708896a47eafee598ec6a02
Paw565 <paw565pl@gmail.com>
2023-08-05 19:17:30 +0200

    fix(ToiletMarker) - changed classes order

--- src/components/Map/ToiletMarker.tsx
+++ src/components/Map/ToiletMarker.tsx
@@ -20,7 +20,7 @@ const CustomMarker = ({
   return (
     <Marker position={{ lat, lng }} icon={markerIcon}>
       <Popup>
-        <h2 className="text-xl mb-1 font-bold">{name}</h2>
+        <h2 className="mb-1 text-xl font-bold">{name}</h2>
         <div>{price === 0 ? "Free" : `${price} PLN`}</div>
         <div>
           <Rating
@@ -31,7 +31,7 @@ const CustomMarker = ({
             transition={true}
           />
         </div>
-        <div className="flex justify-around mt-2">
+        <div className="mt-2 flex justify-around">
           <div>
             <a
               href={`https://www.google.com/maps/dir/?api=1&destination=${lat}%2C${lng}`}
