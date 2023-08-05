b0616930813f545a6ff9b29ebb93d0d3ae077c72
Paw565 <paw565pl@gmail.com>
2023-08-05 13:02:11 +0200

    created logo component

---
+++ src/components/Navbar/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <h1 className="font-bold text-orange-900 sm:text-3xl sm:tracking-wide cursor-pointer">
+      FreeToilet
+    </h1>
+  );
+};
+
+export default Logo;
--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,15 +1,14 @@
 import { IconContext } from "react-icons";
 import AboutModal from "./AboutModal";
 import AccountModal from "./AccountModal";
+import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
     <IconContext.Provider value={{ size: "1.5em" }}>
       <div className="flex items-center gap-2 bg-slate-50 px-1 py-4 shadow-md sm:px-5">
-        <h1 className="font-bold text-orange-900 sm:text-3xl sm:tracking-wide">
-          FreeToilet
-        </h1>
+        <Logo />
         <SearchBar
           onSubmit={(locationText) => console.log(locationText)}
         ></SearchBar>
