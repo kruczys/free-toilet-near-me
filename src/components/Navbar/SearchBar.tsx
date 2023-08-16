dc0f424ea40bc7800c56a63a492ebc37026ff67d
Paw565 <paw565pl@gmail.com>
2023-08-16 15:05:39 +0200

    fix(navbar): minor styles adjustment

--- src/components/Navbar/SearchBar.tsx
+++ src/components/Navbar/SearchBar.tsx
@@ -17,7 +17,7 @@ const SearchBar = () => {
   return (
     <form
       onSubmit={handleSubmit(submitAction)}
-      className="relative flex w-full items-center"
+      className="ml-1 flex w-full items-center"
     >
       <button
         type="submit"
@@ -28,7 +28,7 @@ const SearchBar = () => {
       </button>
       <input
         {...register("locationSearch")}
-        className="w-full rounded-xl border-2 px-2 py-1 pl-9 focus:border-black focus:outline-none"
+        className="w-full rounded-xl border-2 pr-3 py-1 pl-10 focus:border-black focus:outline-none"
         type="text"
         id="locationSearch"
         placeholder="type your location here..."
